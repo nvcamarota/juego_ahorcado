a7e5561c3b0b4c9eeb5ed04043f5f7ccbb5a35df
Nazarena Camarota <nvcamarota.dev@gmail.com>
2025-01-21 13:27:56 -0300

    Consigna para desarrollar el juego: Adivina la palabra

---
+++ juego_ahorcado.py
@@ -0,0 +1,11 @@
+"""
+1° Proyecto Integrador:
+ADIVINA LA PALABRA
+
+El objetivo consiste en desarrollar el juego interactivo "Adivina la palabra".
+El funcionamiento esperado es el siguiente:
+1. Al ejecutar el programa se mostrará por pantalla una palabra oculta, usando tantos guiones como letras que contiene la palabra a adivinar (la palabra a adivinar será elegida por el programa usando el módulo de Python "random"), la cantidad de vidas con las que cuenta el jugador y las cantidades de letras incorrectas que van ingresando.
+2. Cuando el jugador ingresa una letra es necesario que se valide el dato (que sea una letra). Luego de validar la letra ingresada se corrobora si la letra ingresada pertenece a alguna de las letras de la palabra a adivinar.
+3. Cada vez que el jugador ingrese una letra que NO pertenece a la palabra a adivinar se restará una vida.
+4. El juego finaliza cuando el jugador queda sin vidas, cuando adivina todas las letras de la palabra o cuando selecciona no jugar más. Para todos los casos, se debe mostrar un mensaje indicando si ganó la partida o si perdió.
+"""
